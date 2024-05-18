b3647abba44645d3808620add9789c3df0b05c3a
mdsafaet <mdsafaet84@gmail.com>
2024-05-19 05:02:52 +0600

    1st commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+///Route::get('/products', function () {
+ //   return view('products');
+//});
+
+Route::get('/product_details', function () {
+    return view('product_details');
+});
+
+Route::get('/account', function () {
+    return view('account');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+Route::resource('/products',ProductController::class);
+Route::resource('/users',UserController::class);
+
